cba1dfd79003dbbc6322d6fa785c384c5ee3bfd3
ericpanitz <ericpanitz2004@gmail.com>
2023-04-25 21:55:24 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,11 @@
+const nome = prompt("Nome")
+const idade = Number(prompt("Idade"))
+const maiorDeIdade = idade > 18;
+const idadeEm2050 = 2050 - 2023
+
+console.log(idadeEm2050);
+console.log("nome", nome);
+console.log("idade", idade)
+console.log("idadeEm2050", idadeEm2050);
+
+console.log(new Date().getFullYear());
